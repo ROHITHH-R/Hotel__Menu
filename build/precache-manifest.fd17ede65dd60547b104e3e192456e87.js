d486c6d85bebd47359b182cfd96c32f68b52ceaf
ROHITHH-R <103122749+ROHITHH-R@users.noreply.github.com>
2022-04-07 11:31:16 +0530

    Add files via upload

---
+++ build/precache-manifest.fd17ede65dd60547b104e3e192456e87.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "e121140ef9818e2f000d",
+    "url": "/Hotel_Menu/static/css/main.565bf1fb.chunk.css"
+  },
+  {
+    "revision": "e121140ef9818e2f000d",
+    "url": "/Hotel_Menu/static/js/main.e121140e.chunk.js"
+  },
+  {
+    "revision": "836e4380fe3f7816a983",
+    "url": "/Hotel_Menu/static/js/1.836e4380.chunk.js"
+  },
+  {
+    "revision": "b68ff5d7436872c95e77",
+    "url": "/Hotel_Menu/static/js/2.b68ff5d7.chunk.js"
+  },
+  {
+    "revision": "cb41d3fc595fee5df68e",
+    "url": "/Hotel_Menu/static/js/runtime~main.cb41d3fc.js"
+  },
+  {
+    "revision": "c6064495dbcc18432508dd8cc9a0b24e",
+    "url": "/Hotel_Menu/static/media/green.c6064495.jpg"
+  },
+  {
+    "revision": "3d793882b718945d5c78073765443160",
+    "url": "/Hotel_Menu/static/media/dose.3d793882.jpg"
+  },
+  {
+    "revision": "71008f64543f8998ccf6e541c748ca38",
+    "url": "/Hotel_Menu/static/media/rice.71008f64.jpg"
+  },
+  {
+    "revision": "9169f29d6ffbad4839437efb29da0c94",
+    "url": "/Hotel_Menu/static/media/vegbiryani.9169f29d.jpg"
+  },
+  {
+    "revision": "2d299bbf050dfe14c534b6ee7350ec5f",
+    "url": "/Hotel_Menu/static/media/samberice.2d299bbf.jpg"
+  },
+  {
+    "revision": "74a9f30286304c0c91b8b40da25a4247",
+    "url": "/Hotel_Menu/static/media/fruit.74a9f302.jpg"
+  },
+  {
+    "revision": "fe9c63b4f3226bde991821240b036cd7",
+    "url": "/Hotel_Menu/static/media/poori.fe9c63b4.jpg"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/Hotel_Menu/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "4deef2a585da9389733b5b77dbbc8fce",
+    "url": "/Hotel_Menu/index.html"
+  }
+];
